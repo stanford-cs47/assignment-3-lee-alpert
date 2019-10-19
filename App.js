9e05bda8b384f0a0d31aabcfa4f2afdb77ec8af4
Lee <a-lee12@hotmail.com>
2019-10-18 21:50:16 -0700

    assignment 3

--- App.js
+++ App.js
@@ -8,12 +8,21 @@
 */
 
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import { Images, Colors } from './App/Themes'
+import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, TouchableHighlight, FlatList, ActivityIndicator, Keyboard, Linking} from 'react-native';
+import { Images, Colors, Metrics } from './App/Themes'
 import APIRequest from './App/Config/APIRequest'
+import { Icon } from 'react-native-elements'
 
 import News from './App/Components/News'
 import Search from './App/Components/Search'
+
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
 
 export default class App extends React.Component {
 
@@ -25,9 +34,8 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-
     //uncomment this to run an API query!
-    //this.loadArticles();
+    this.loadArticles();
   }
 
   async loadArticles(searchTerm = '', category = '') {
@@ -42,18 +50,80 @@ export default class App extends React.Component {
     this.setState({loading: false, articles: resultArticles})
   }
 
+_handleTextChange = inputValue => {
+    this.setState({ searchText: inputValue });
+  };
+
+_fetchResults = () => {
+  this.loadArticles(this.state.searchText)
+  this.setState({ searchText: "" });
+}
+
+getArticleContent = () => {
+  const{articles, loading} = this.state;
+
+  let contentDisplayed = null;
+
+  if (loading) {
+    contentDisplayed = <ActivityIndicator style = {[styles.flatListView]}/>;
+  }
+
+  else {
+    contentDisplayed = <FlatList 
+          data = {articles}
+          renderItem={({ item }) => (
+              <View>
+                <TouchableHighlight onPress={() => Linking.openURL(item.url)}>
+                  <View>
+                    <Text style={styles.articleTitle}>{item.title}</Text>
+                  </View>
+                </TouchableHighlight>
+                <Text>{item.snippet}</Text>
+                <Text style={styles.articleAuthor}>{item.byline}</Text>
+                <Text style={styles.articleDate}>{item.date}</Text>
+              </View>
+            )}
+          keyExtractor={item => item.url} 
+          />
+  }
+
+  return ( 
+    <View>
+      {contentDisplayed}
+    </View>
+  )
+}
+
   render() {
     const {articles, loading} = this.state;
-
     return (
       <SafeAreaView style={styles.container}>
+        <View style={[styles.logo]}>
+          <Image
+              style={{height: Metrics.images.logo, width: Metrics.screenWidth, resizeMode:'contain'}}
+              source={Images.logo}
+            />
+        </View>
 
-        <Text style={{textAlign: 'center'}}>Have fun! :) {"\n"} Start by changing the API Key in "./App/Config/AppConfig.js" {"\n"} Then, take a look at the following components: {"\n"} NavigationButtons {"\n"} Search {"\n"} News {"\n"} 🔥</Text>
+        <View style={{flexDirection:'row', width: Metrics.screenWidth, alignItems:'center', justifyContent:'center',}}>
+          <View style={{flex: 2, width: Metrics.screenWidth, marginLeft: Metrics.screenWidth/20, marginRight: Metrics.screenWidth/20}}>
+            <TextInput
+              style={[styles.textInput]}
+              placeholder="Search for News"
+              onChangeText={(text) => this._handleTextChange(text)}
+              value={this.state.searchText}
+            />
+          </View>
+          <TouchableHighlight style={[styles.magnifyingGlass]} onPress = {()=>this._fetchResults()} underlayColor = 'transparent'>
+            <View>
+              <Icon name="search" size = {30} color = "#F02484" />
+            </View>
+          </TouchableHighlight>
+        </View>
 
-        {/*First, you'll need a logo*/}
-
-        {/*Then your search bar*/}
-
+        <View style={[styles.flatListView]}>
+          {this.getArticleContent()}
+        </View>
         {/*And some news*/}
 
         {/*Though, you can style and organize these however you want! power to you 😎*/}
@@ -70,6 +140,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+  },
+  logo: {
+    flex: 1,
+    flexDirection:'row',
+  },
+  textInput: {
+    height: Metrics.navBarHeight,
+    width: Metrics.screenWidth - Metrics.screenWidth/10,
+    backgroundColor: Colors.frost,
+    borderRadius: 15,
+    padding: 20, 
+  },
+  magnifyingGlass: {
+    alignItems:'center',
+    justifyContent:'center',
+    flex: 1,
+  },
+  flatListView: {
+    alignItems:'center',
+    justifyContent:'center',
+    flex: 4,
+    flexDirection: 'row',
+    padding: 25,
+  },
+  articleTitle: {
+    fontSize: 30,
+    marginTop: 20,
+  },
+  articleAuthor: {
+    fontWeight: 'bold',
+    marginTop: 5,
+    marginBottom: 5,
+  },
+  articleDate: {
+    color: Colors.charcoal,
   }
 });
